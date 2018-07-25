80084d35752907b18b7600a6b0da6fef8717efff
michael <michaelcdavids@gmail.com>
2018-07-25 11:46:05 +0200

    added more tests

---
+++ test/greet.test.js
@@ -0,0 +1,13 @@
+let greet = require('../greet');
+let assert = require('assert');
+
+describe('The greet function', function(){
+
+    it('should greet Andrew correctly', function(){
+        assert.equal('Hello, Andrew', greet('Andrew'));
+    });
+    it('should greet Michael correctly', function(){
+        assert.equal('Hello, Michael', greet('Michael'));
+    });
+
+});
